06838b69a4b0f434b96e73c7fac10750c392e3ef
Pedro León <pedro@wintercr.com>
2024-10-01 16:53:34 -0500

    Deleted prisma, i will try typeorm instead

--- app.mjs
+++ app.mjs
@@ -1,35 +1,13 @@
 import express from "express"
 import cors from "cors"
-import { PrismaClient } from "@prisma/client"
 
 const app = express();
-const prisma = new PrismaClient();
 app.use(cors());
 app.use(express.json());
 
 
 app.get("/", async (req, res) => {
-  const user = await prisma.user.create({
-    data: {
-      name: 'Alice',
-      email: 'alice@prisma.io',
-      posts: {
-        create: { title: 'Hello World' },
-      },
-      profile: {
-        create: { bio: 'I like turtles' },
-      },
-    },
-  })
-
-  const allUsers = await prisma.user.findMany({
-    include: {
-      posts: true,
-      profile: true,
-    },
-  })
-
-  return res.json(allUsers)
+  return res.send("Hello world!")
 })
 
 
