a05499ec1deebf9b23f1bba8db97db5f5c7de7ce
Pedro León <pedro@wintercr.com>
2024-09-30 19:14:28 -0500

    Tinkering with prisma

--- app.mjs
+++ app.mjs
@@ -1,13 +1,35 @@
 import express from "express"
 import cors from "cors"
+import { PrismaClient } from "@prisma/client"
 
 const app = express();
+const prisma = new PrismaClient();
 app.use(cors());
 app.use(express.json());
 
 
-app.get("/", (req, res) => {
-  return res.send("Hello world!")
+app.get("/", async (req, res) => {
+  const user = await prisma.user.create({
+    data: {
+      name: 'Alice',
+      email: 'alice@prisma.io',
+      posts: {
+        create: { title: 'Hello World' },
+      },
+      profile: {
+        create: { bio: 'I like turtles' },
+      },
+    },
+  })
+
+  const allUsers = await prisma.user.findMany({
+    include: {
+      posts: true,
+      profile: true,
+    },
+  })
+
+  return res.json(allUsers)
 })
 
 
